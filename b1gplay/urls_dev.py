8da855e70b97017532745761cbbee6b399029b38
nathanbaleeta <nbaleeta@gmail.com>
2019-12-22 21:12:43 +0300

    Edited title for swagger ui

--- b1gplay/urls_dev.py
+++ b1gplay/urls_dev.py
@@ -10,7 +10,7 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Symbo Board API",
+        title="B1gplay API",
         default_version='v1',
         description="Test description",
         terms_of_service="https://www.google.com/policies/terms/",
